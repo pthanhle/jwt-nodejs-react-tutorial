ea3c9b0f8dcc05c2f95b0b9c200e47327de2a7ce
pthanhle <22110228@student.hcmute.edu.vn>
2025-04-01 15:56:42 +0700

    #5.1 Hash User Password

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,11 +1,4 @@
-import mysql from 'mysql2';
-
-//create connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'jwt'
-})
+import userService from "../service/userService"
 
 const handleHelloWorld = (req, res) => {
     return res.render("home.ejs");
@@ -19,14 +12,9 @@ const handleCreateNewUser = (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
     let username = req.body.username;
-    connection.query(
-        'INSERT INTO users(email,password,username) VALUES(?, ?, ?)', [email, password, username],
-        function (err, results, fields) {
-            if (err) {
-                console.log(err)
-            }
-        }
-    )
+
+    // userService.createNewUser(email, password, username);
+    userService.getUserList();
     return res.send("handleCreateNewUser")
 }
 
---
+++ src/service/userService.js
@@ -0,0 +1,46 @@
+import bcrypt from 'bcryptjs';
+import mysql from 'mysql2';
+
+const salt = bcrypt.genSaltSync(10);
+
+//create connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt'
+})
+
+const hashUserPassword = (userPassword) => {
+    let hashPassword = bcrypt.hashSync(userPassword, salt)
+    return hashPassword
+}
+
+const createNewUser = (email, password, username) => {
+    let hashPass = hashUserPassword(password);
+    connection.query(
+        'INSERT INTO users(email,password,username) VALUES(?, ?, ?)', [email, hashPass, username],
+        function (err, results, fields) {
+            if (err) {
+                console.log(err)
+            }
+        }
+    )
+}
+
+const getUserList = () => {
+    let users = [];
+    connection.query(
+        'Select * from users',
+        function (err, results, fields) {
+            if (err) {
+                console.log(err)
+            }
+            console.log("check res: ", results);
+        }
+    )
+}
+
+module.exports = {
+    createNewUser,
+    getUserList
+}
