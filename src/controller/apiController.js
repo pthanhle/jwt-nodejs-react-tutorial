12954a78827761a9113892a0db8ecaa949518fcb
pthanhle <22110228@student.hcmute.edu.vn>
2025-09-09 23:14:20 +0700

    done Register

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,3 +1,5 @@
+import loginRegisterService from '../service/loginRegisterService'
+
 const testApi = (req, res) => {
     return res.status(200).json({
         message: 'ok',
@@ -5,6 +7,41 @@ const testApi = (req, res) => {
     })
 }
 
+const handleRegister = async (req, res) => {
+    try {
+        //req.body: email, phone,username, password
+        if (!req.body.email || !req.body.phone || !req.body.password) {
+            return res.status(200).json({
+                EM: 'Missing required parameters', //error message
+                EC: '1', //error code
+                DT: '', //data
+            })
+        }
+        if (req.body.password && req.body.password.length < 5) {
+            return res.status(200).json({
+                EM: 'Your password must have more than 5 letters', //error message
+                EC: '1', //error code
+                DT: '', //data
+            })
+        }
+
+        //service: create user
+        let data = await loginRegisterService.registerNewUser(req.body)
+        return res.status(200).json({
+            EM: data.EM, //error message
+            EC: data.EC, //error code
+            DT: '', //data
+        })
+
+    } catch (e) {
+        return res.status(500).json({
+            EM: 'error form server', //error message
+            EC: '-1', //error code
+            DT: '', //data
+        })
+    }
+}
+
 module.exports = {
-    testApi
+    testApi, handleRegister
 }
--- src/server.js
+++ src/server.js
@@ -1,32 +1,17 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
+import initApiRoutes from "./routes/api";
 require("dotenv").config();
 import bodyParser from 'body-parser';
+import configCors from "./config/cors";
 // import connection from "./config/connectDB";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+//config cors
+configCors(app)
 
 //config view engine
 configViewEngine(app);
@@ -40,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //init web routers
 initWebRoutes(app);
+initApiRoutes(app);
 
 app.listen(PORT, () => {
     console.log(">>> JWT Backend is running on the port = " + PORT);
