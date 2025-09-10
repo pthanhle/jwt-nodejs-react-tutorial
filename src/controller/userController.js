edda2bc29036d19547e822518e847987eaebc7e8
pthanhle <22110228@student.hcmute.edu.vn>
2025-09-10 17:17:24 +0700

    delete user

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,7 +1,6 @@
 import userApiService from '../service/userApiService'
 
 const readFunc = async (req, res) => {
-
     try {
         if (req.query.page && req.query.limit) {
             let page = req.query.page
@@ -56,15 +55,20 @@ const updateFunc = (req, res) => {
     }
 }
 
-const deleteFunc = (req, res) => {
+const deleteFunc = async (req, res) => {
     try {
-
+        let data = await userApiService.deleteUser(req.body.id);
+        return res.status(200).json({
+            EM: data.EM, //error message
+            EC: data.EC, //error code
+            DT: data.DT, //data
+        })
     } catch (error) {
-        return {
-            EM: '',
-            EC: -1,
-            DT: ''
-        }
+        return res.status(500).json({
+            EM: 'error form server', //error message
+            EC: '1', //error code
+            DT: '', //data
+        })
     }
 }
 
