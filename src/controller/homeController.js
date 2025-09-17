013e310e8e588056a5ce0c5f59d203e5bb8def41
pthanhle <22110228@student.hcmute.edu.vn>
2025-09-17 18:55:12 +0700

    JWT Middleware

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -6,7 +6,7 @@ const handleHelloWorld = (req, res) => {
 
 const handleUserPage = async (req, res) => {
     // Cookies that have not been signed
-    console.log('Cookies: ', req.cookies)
+    // console.log('Cookies: ', req.cookies)
 
     let userList = await userService.getUserList();
     return res.render("user.ejs", { userList })
