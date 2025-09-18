42bc17af5451a9fa41555a2c2815ba73e61644c5
pthanhle <22110228@student.hcmute.edu.vn>
2025-09-18 17:11:09 +0700

    Bearer Authentication

--- src/middleware/JWTAction.js
+++ src/middleware/JWTAction.js
@@ -8,8 +8,13 @@ const createJWT = (payload) => {
     let key = process.env.JWT_SECRET
     let token = null
     try {
-        token = jwt.sign(payload, key);
-    } catch (error) {
+        token = jwt.sign(
+            payload,
+            key,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+        );
+    }
+    catch (error) {
         console.log(error)
     }
     return token;
@@ -27,12 +32,21 @@ const verifyToken = (token) => {
     return decoded;
 }
 
+const extractToken = (req) => {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    }
+    return null;
+}
+
 const checkUserJWT = (req, res, next) => {
     if (nonSecurePaths.includes(req.path)) return next();
 
     let cookies = req.cookies
-    if (cookies && cookies.jwt) {
-        let token = cookies.jwt;
+    let tokenFromHeader = extractToken(req)
+
+    if ((cookies && cookies.jwt) || tokenFromHeader) {
+        let token = cookies && cookies.jwt ? cookies.jwt : tokenFromHeader;
         let decoded = verifyToken(token)
         if (decoded) {
             req.user = decoded
@@ -45,7 +59,8 @@ const checkUserJWT = (req, res, next) => {
                 EM: 'Not authenticated the user!'
             })
         }
-    } else {
+    }
+    else {
         return res.status(401).json({
             EC: -1,
             DT: '',
