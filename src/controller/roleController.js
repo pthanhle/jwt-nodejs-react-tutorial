ea8923192a474735e3c0f6575c351889af67a58e
pthanhle <22110228@student.hcmute.edu.vn>
2025-09-20 17:56:09 +0700

    api read/delete roles

--- src/controller/roleController.js
+++ src/controller/roleController.js
@@ -3,27 +3,14 @@ import roleApiService from '../service/roleApiService'
 
 const readFunc = async (req, res) => {
     try {
-        if (req.query.page && req.query.limit) {
-            let page = req.query.page
-            let limit = req.query.limit
-
-            let data = await userApiService.getUserWithPagination(+page, +limit)
-            return res.status(200).json({
-                EM: data.EM, //error message
-                EC: data.EC, //error code
-                DT: data.DT, //data
-            })
-
-        } else {
-
-            let data = await userApiService.getAllUser()
-            return res.status(200).json({
-                EM: data.EM, //error message
-                EC: data.EC, //error code
-                DT: data.DT, //data
-            })
-        }
-    } catch (error) {
+        let data = await roleApiService.getAllRoles()
+        return res.status(200).json({
+            EM: data.EM, //error message
+            EC: data.EC, //error code
+            DT: data.DT, //data
+        })
+    }
+    catch (error) {
         return res.status(500).json({
             EM: 'error form server', //error message
             EC: '1', //error code
@@ -71,7 +58,7 @@ const updateFunc = async (req, res) => {
 
 const deleteFunc = async (req, res) => {
     try {
-        let data = await userApiService.deleteUser(req.body.id);
+        let data = await roleApiService.deleteRoles(req.body.id);
         return res.status(200).json({
             EM: data.EM, //error message
             EC: data.EC, //error code
--- src/service/roleApiService.js
+++ src/service/roleApiService.js
@@ -1,10 +1,11 @@
+import { where } from "sequelize/lib/sequelize"
 import db from "../models/index"
 
 const createNewRoles = async (roles) => {
     try {
         let currentRoles = await db.Role.findAll({
             attributes: ['url', 'description'],
-            raw: true
+            raw: true,
         })
 
         const persists = roles.filter(({ url: url1 }) =>
@@ -33,6 +34,49 @@ const createNewRoles = async (roles) => {
     }
 }
 
+const getAllRoles = async () => {
+    try {
+        let data = await db.Role.findAll({
+            order: [['id', 'DESC']]
+        })
+        return {
+            EM: `Get all roles success`,
+            EC: 0,
+            DT: data
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            EM: 'something wrongs with service',
+            EC: 1,
+            DT: []
+        }
+    }
+}
+
+const deleteRoles = async (id) => {
+    try {
+        let role = await db.Role.findOne({
+            where: { id: id }
+        })
+        if (role) {
+            await role.destroy();
+        }
+        return {
+            EM: `Delete roles success`,
+            EC: 0,
+            DT: []
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            EM: 'something wrongs with service',
+            EC: 1,
+            DT: []
+        }
+    }
+}
+
 module.exports = {
-    createNewRoles
+    createNewRoles, getAllRoles, deleteRoles
 }
